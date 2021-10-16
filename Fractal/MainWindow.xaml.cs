45748e204367eacfbbf2d02b401b24ec4869cc35
WEBMaximWEB <maxim.geer@yandex.ru>
2021-10-16 08:04:01 +0500

    0.1.0 L-система работает
    Убрал все лишнее, исправил ошибки

--- Fractal/MainWindow.xaml.cs
+++ Fractal/MainWindow.xaml.cs
@@ -15,22 +15,14 @@ using System.Windows.Shapes;
 
 namespace Fractal
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         static List<Point> Points = new List<Point>();
-        static int Deep = 10;
+        static int Deep = 8;
 
         public MainWindow()
         {
             InitializeComponent();
-/*            int x = 20;
-            int y = 20;
-            Points.Add(new Point(x, y));
-            Fractal(1, 800, x, y, 4);
-            Draw(Brushes.Green, 2);*/
 
             string condition1 = "LbFRAFARFbL";
             string condition2 = "RAFLBFBLFAR";
@@ -58,91 +50,6 @@ namespace Fractal
             }
         }
 
-        void Fractal(int deraction, int len, double x, double y, int Deep)
-        {
-            Deep--;
-            if(deraction == 1)
-            {
-                if (Deep <= 1)   //true
-                {
-                    Points.Add(new Point(x, y + len));
-                    Points.Add(new Point(x + len, y + len));
-                    Points.Add(new Point(x + len, y));
-                    return;
-                }
-                else  //true
-                {
-                    Fractal(4, len / 3, x, y, Deep);
-                    Points.Add(new Point(x, y + ((len / 3) * 2)));
-                    Fractal(1, len / 3, x, y + ((len / 3) * 2), Deep);
-                    Points.Add(new Point(x + ((len / 3) * 2), y + ((len / 3) * 2)));
-                    Fractal(1, len / 3, x + ((len / 3) * 2), y + ((len / 3) * 2), Deep);
-                    Points.Add(new Point(x + len, y + len / 3));
-                    Fractal(2, len / 3, x + len, y + len / 3, Deep);
-                }
-            }
-            else if(deraction == 2)
-            {
-                if(Deep <= 1) //true
-                {
-                    Points.Add(new Point(x - len, y));
-                    Points.Add(new Point(x - len, y - len));
-                    Points.Add(new Point(x, y - len));
-                    return;
-                }
-                else
-                {
-                    Fractal(3, len / 3, x, y, Deep);
-                    Points.Add(new Point(x - ((len / 3) * 2), y));
-                    Fractal(2, len / 3, x - ((len / 3) * 2), y, Deep);
-                    Points.Add(new Point(x - ((len / 3) * 2), y - ((len / 3) * 2)));
-                    Fractal(2, len / 3, x - (len / 3) * 2, y - ((len / 3) * 2), Deep);
-                    Points.Add(new Point(x - (len / 3), y - len));
-                    Fractal(1, len / 3, x - (len / 3), y - len, Deep);
-                }
-            }
-            else if (deraction == 3)
-            {
-                if(Deep <= 1)  //true
-                {
-                    Points.Add(new Point(x, y - len));
-                    Points.Add(new Point(x - len, y - len));
-                    Points.Add(new Point(x - len, y));
-                }
-                else
-                {
-                    Fractal(2, len / 3, x, y, Deep);
-                    Points.Add(new Point(x, y - ((len / 3) * 2)));
-                    Fractal(3, len / 3, x, y - ((len / 3) * 2), Deep);
-                    Points.Add(new Point(x - ((len / 3) * 2), y - ((len / 3) * 2)));
-                    Fractal(3, len / 3, x - ((len / 3) * 2), y - ((len / 3) * 2), Deep);
-                    Points.Add(new Point(x - len, y - (len / 3)));
-                    Fractal(4, len / 3, x - len, y - (len / 3), Deep);
-
-                }
-            }
-            else if (deraction == 4)
-            {
-                if(Deep <= 1)  //true
-                {
-                    Points.Add(new Point(x + len, y));
-                    Points.Add(new Point(x + len, y + len));
-                    Points.Add(new Point(x, y + len));
-                    return;
-                }
-                else  //true
-                {
-                    Fractal(1, len / 3, x, y, Deep);
-                    Points.Add(new Point(x + ((len / 3) * 2), y));
-                    Fractal(4, len / 3, x + ((len / 3) * 2), y, Deep);
-                    Points.Add(new Point(x + ((len / 3) * 2), y + ((len / 3) * 2)));
-                    Fractal(4, len / 3, x + ((len / 3) * 2), y + ((len / 3) * 2), Deep);
-                    Points.Add(new Point(x + (len / 3), y + len));
-                    Fractal(3, len / 3, x + (len / 3), y + len, Deep);
-                }
-            }
-        }
-
         string LGenerator(string input, string condition1, string condition2)
         {
             input = input.Replace("A", condition1);
@@ -153,14 +60,15 @@ namespace Fractal
 
         string DeleteConditions(string input)
         {
-            input.Replace("A", string.Empty);
-            return input.Replace("B", string.Empty);
+            input = input.Replace("A", string.Empty);
+            input = input.Replace("B", string.Empty);
+            return input;
         }
 
         void LParser(string str, double len)
         {
             int direction = 40;
-            Points.Add(new Point(800, 150));
+            Points.Add(new Point(500, 150));
             foreach(char c in str)
             {
                 if (c == 'L')
