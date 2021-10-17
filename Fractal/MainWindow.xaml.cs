0d446150b24ce3d1a621f311cd7bfbb17c019ebc
IlyaIIS <ilya1221221@mail.ru>
2021-10-17 22:12:16 +0500

    0.3.0 Фрактальное дерево
    > Добавлено фрактальное дерево
    > Оба фрактальных алгоритма вынесены в отдельные классы

--- Fractal/MainWindow.xaml.cs
+++ Fractal/MainWindow.xaml.cs
@@ -17,43 +17,79 @@ namespace Fractal
 {
     public partial class MainWindow : Window
     {
-        static List<Point> Points = new List<Point>();
-        static int Deep = 5;
-
         public MainWindow()
         {
             InitializeComponent();
 
+            CalculateAndDrawFractal(FractalName.Tree);
+        }
+
+        void CalculateAndDrawFractal(FractalName fractalName)
+        {
+            switch (fractalName)
+            {
+                case FractalName.HilbertCurve:
+                    HilbertCurve hilbertCurvefractal = new HilbertCurve(5);
+                    hilbertCurvefractal.CalculateAndDraw(ref Field);
+                    break;
+                case FractalName.Tree:
+                    TreeFractal treefractal = new TreeFractal(3, 3, 100);
+                    treefractal.CalculateAndDraw(ref Field, Width, Height);
+                    break;
+            }
+        }
+
+        enum FractalName
+        {
+            HilbertCurve,
+            Tree
+        }
+    }
+
+    class HilbertCurve
+    {
+        public readonly int Deep;
+        List<Point> points;
+
+        public HilbertCurve(int deep)
+        {
+            Deep = deep;
+        }
+
+        public void CalculateAndDraw(ref Canvas canvas)
+        {
+            points = new List<Point>();
+
             string condition1 = "LbFRAFARFbL";
             string condition2 = "RAFLBFBLFAR";
             string str = "A";
-            str = LGenerator(str, condition1, condition2);
+            str = LGenerator(str, condition1, condition2, Deep);
             str = DeleteConditions(str);
             LParser(str, 15);
-            Draw(Brushes.Green, 2);
+            Draw(ref canvas, Brushes.Green, 2);
         }
 
-        void Draw(Brush color, int thickness)
+        void Draw(ref Canvas canvas, Brush color, int thickness)
         {
-            for (int i = 1; i < Points.Count; i++)
+            for (int i = 1; i < points.Count; i++)
             {
-                Field.Children.Add(new Line()
+                canvas.Children.Add(new Line()
                 {
-                    X1 = Points[i - 1].X,
-                    X2 = Points[i].X,
-                    Y1 = Points[i - 1].Y,
-                    Y2 = Points[i].Y,
+                    X1 = points[i - 1].X,
+                    X2 = points[i].X,
+                    Y1 = points[i - 1].Y,
+                    Y2 = points[i].Y,
                     StrokeThickness = thickness,
                     Stroke = color
                 });
             }
         }
 
-        string LGenerator(string input, string condition1, string condition2)
+        string LGenerator(string input, string condition1, string condition2, int deep)
         {
-            Deep--;
-            if (Deep >= 0)
-                input = LGenerator(input, condition1, condition2);
+            deep--;
+            if (deep >= 0)
+                input = LGenerator(input, condition1, condition2, deep);
             input = input.Replace("A", condition1);
             input = input.Replace("B", condition2);
             input = input.Replace("b", "B");
@@ -70,15 +106,15 @@ namespace Fractal
         void LParser(string str, double len)
         {
             int direction = 40;
-            Points.Add(new Point(500, 150));
-            foreach(char c in str)
+            points.Add(new Point(5, 5));
+            foreach (char c in str)
             {
                 if (c == 'L')
                     direction--;
                 else if (c == 'R')
                     direction++;
                 else
-                    Points.Add(AddPoint(Points[^1].X, Points[^1].Y, direction, len));
+                    points.Add(AddPoint(points[^1].X, points[^1].Y, direction, len));
             }
         }
 
@@ -88,10 +124,73 @@ namespace Fractal
             if (direction == 1)
                 return new Point(x, y - len);
             else if (direction == 2)
-                return new Point(x + len, y);
+                return new Point(x - len, y);
             else if (direction == 3)
                 return new Point(x, y + len);
-            return new Point(x - len, y);
+            return new Point(x + len, y);
+        }
+    }
+
+    class TreeFractal
+    {
+        public readonly int BranchNum; //3
+        public readonly int Depth; //5
+        public readonly double Length; //30
+        public readonly double AngleIndent;
+        public readonly double AngleDigression;
+        public readonly double AngleInc;
+        List<Line> lines;
+
+        public TreeFractal(int branchNum, int depth, double length)
+        {
+            BranchNum = branchNum;
+            Depth = depth;
+            Length = length;
+
+            AngleIndent = Math.PI / 5;
+            double angle = Math.PI - AngleIndent * 2;
+            AngleDigression = angle / (BranchNum + 1);
+            AngleInc = angle / (BranchNum + 1);
+        }
+        public void CalculateAndDraw(ref Canvas canvas, double winWidth, double winHeight)
+        {
+            lines = new List<Line>();
+
+            DoFactorial(new Point(winWidth * 0.5, winHeight * 0.9), Math.PI / 2, 1);
+
+            SetLinesOnCanvas(ref canvas);
+        }
+
+        void DoFactorial(Point lastPoint, double angle, int depth)
+        {
+            for (int i = 0; i < BranchNum; i++)
+            {
+                double currentAngle = angle - Math.PI / 2 + AngleIndent + AngleDigression + AngleInc * i;
+                double currentLength = Length * ((Depth - depth + 1d) / Depth);
+                Point currentPoint = new Point(lastPoint.X + Math.Cos(currentAngle) * currentLength, lastPoint.Y - Math.Sin(currentAngle) * currentLength);
+                AddLine(lastPoint, currentPoint);
+                if (depth < Depth)
+                {
+                    DoFactorial(currentPoint, currentAngle, depth + 1);
+                }
+            }
+        }
+        void AddLine(Point point1, Point point2)
+        {
+            Line line = new Line();
+            line.X1 = point1.X;
+            line.Y1 = point1.Y;
+            line.X2 = point2.X;
+            line.Y2 = point2.Y;
+            line.StrokeThickness = 1;
+            line.Stroke = Brushes.Black;
+            lines.Add(line);
+        }
+
+        void SetLinesOnCanvas(ref Canvas canvas)
+        {
+            foreach(Line line in lines)
+                canvas.Children.Add(line);
         }
     }
 }
